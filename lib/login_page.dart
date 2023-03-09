72ed2a53a71bef320246faa9807ef161443f559c
xsheed <rasyiddesu@gmail.com>
2023-03-10 02:30:38 +0700

    tugas1

---
+++ lib/login_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Page"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const LogoImageAssets(),
+            const Padding(padding: EdgeInsets.only(top: 20.0)),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                ),
+                hintText: "Username",
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 20.0)),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                ),
+                hintText: "Password",
+              ),
+              obscureText: true,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 40.0),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                MaterialButton(
+                  child: const Text("Login"),
+                  color: Colors.blue,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 20.0),
+            ),
+            TextButton(onPressed: () {}, child: const Text("Forgot Password"))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LogoImageAssets extends StatelessWidget {
+  const LogoImageAssets({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = const AssetImage('assets/images/flutter.png');
+
+    Image image = Image(image: assetImage, height: 100, width: 100);
+    return Container(
+      child: image,
+    );
+  }
+}
